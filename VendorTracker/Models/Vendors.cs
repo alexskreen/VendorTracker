325c3a67618836f4aa030fa6360010aba78254fc
Alex Skreen <skreenalex@gmail.com>
2020-03-13 09:26:04 -0700

    add Vendor class

--- VendorTracker/Models/Vendors.cs
+++ VendorTracker/Models/Vendors.cs
@@ -4,5 +4,9 @@ namespace VendorTracker.Models
 {
   public class Vendor
   {
+    private static List<Vendor> _instances = new List<Vendor> {};
+    public string Company { get; set; }
+    public int Id { get; }
+    public List<Order> Orders { get; set; }
   }
 }
