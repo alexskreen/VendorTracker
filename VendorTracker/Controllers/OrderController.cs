d2439f890767b715540de0cd83c17e0527a05861
Alex Skreen <skreenalex@gmail.com>
2020-03-13 12:34:27 -0700

    add route to OrderController

--- VendorTracker/Controllers/OrderController.cs
+++ VendorTracker/Controllers/OrderController.cs
@@ -4,5 +4,13 @@ using System.Collections.Generic;
 
 namespace VendorTracker.Controllers
 {
-
+public class ItemsController : Controller
+  {
+    [HttpGet("/categories/{categoryId}/items/new")]
+    public ActionResult New(int categoryId)
+    {
+      Category category = Category.Find(categoryId);
+      return View(category);
+    }
+  }
 }
--- VendorTracker/Controllers/VendorController.cs
+++ VendorTracker/Controllers/VendorController.cs
@@ -37,5 +37,18 @@ namespace VendorTracker.Controllers
       model.Add("orders", vendorOrders);
       return View(model);
     }
+
+    [HttpPost("/vendors/{vendoryId}/orders")]
+    public ActionResult Create(int vendorId, string orderDetails)
+    {
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      Vendor foundVendor = Vendor.Find(vendorId);
+      Order newOrder = new Order(orderDetails);
+      foundVendor.AddOrder(newOrder);
+      List<Order> vendorOrders = foundVendor.Orders;
+      model.Add("orders", vendorOrders);
+      model.Add("vendor", foundVendor);
+      return View("Show", model);
+    }
   }
 }
