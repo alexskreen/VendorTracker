8e235bcd4656d028ecb934cfbbae9357e2123a1e
Alex Skreen <skreenalex@gmail.com>
2020-03-13 10:46:46 -0700

    add Order class

--- VendorTracker/Models/Orders.cs
+++ VendorTracker/Models/Orders.cs
@@ -2,5 +2,33 @@ using System.Collections.Generic;
 
 namespace VendorTracker.Models
 {
+  public class Order
+  {
+    public string Details { get; set; }
+    public int Id { get; }
+    private static List<Order> _instances = new List<Order> { };
 
+    public Order(string details)
+    {
+      Details = details;
+      _instances.Add(this);
+      Id = _instances.Count;
+    }
+
+    public static List<Order> GetAll()
+    {
+      return _instances;
+    }
+
+    public static void ClearAll()
+    {
+      _instances.Clear();
+    }
+
+    public static Item Find(int searchId)
+    {
+      return _instances[searchId - 1];
+    }
+
+  }
 }
